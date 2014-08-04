5c32f2eb2499d5e45c84e3649383c1939e7242ee
ridwanbejo <ridwanbejo@gmail.com>
2014-08-04 09:09:26 +0700

    menambahkan fitur pencarian kontak

--- src/get_contact.php
+++ src/get_contact.php
@@ -2,12 +2,23 @@
 
 include "connect_db.php";
 
-
-$sql = "SELECT * FROM contact";
-$rows = mysql_query($sql);
 $data = array();
-while($row=mysql_fetch_array($rows)){
-    $data[] = $row;
+
+if (isset($_GET['q'])){
+    $sql = "SELECT * FROM contact where name like '%".$_GET['q']."%'";
+    $rows = mysql_query($sql);
+    while($row=mysql_fetch_array($rows)){
+        $data[] = $row;
+    }
+}
+
+else{
+    $sql = "SELECT * FROM contact";
+    $rows = mysql_query($sql);
+    while($row=mysql_fetch_array($rows)){
+        $data[] = $row;
+    }
+
 }
 
 echo json_encode($data);
